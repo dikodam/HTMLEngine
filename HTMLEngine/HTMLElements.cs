7a0ebc9f1d679c0d07e5dbfa978dc31a4eeb5846
adiko <dikodam@googlemail.com>
2017-05-05 20:47:37 +0200

    Initial commit

---
+++ HTMLEngine/HTMLElements.cs
@@ -0,0 +1,40 @@
+﻿namespace HTML
+{
+    class DocumentType
+    {
+        public override string ToString()
+        {
+            return "<!DOCTYPE html>";
+        }
+    }
+
+    class Html : INested, ITagged
+    {
+
+        public string TagId { get; }
+
+        public object[] Elements { get; }
+
+        public Html()
+        {
+            TagId = "html";
+        }
+
+        public Html(params object[] elements) : base()
+        {
+            Elements = elements;
+        }
+
+        public override string ToString()
+        {
+            string html = "<" + TagId + ">";
+
+            foreach (object element in Elements)
+            {
+                html += element.ToString();
+            }
+
+            return html + "</" + TagId + ">";
+        }
+    }
+}
---
+++ HTMLEngine/HTMLEngine.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace HTML
+{
+    public interface ITagged
+    {
+        string TagId { get; }
+    }
+
+    public interface INested
+    {
+        object[] Elements { get; }
+    }
+
+    public static class Engine
+    {
+        public static string Generate(params object[] elements)
+        {
+            string html = "";
+            foreach (object element in elements)
+            {
+                html += element.ToString();
+            }
+            return html;
+        }
+    }
+}
